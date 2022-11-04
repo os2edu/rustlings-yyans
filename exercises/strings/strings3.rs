f8f9bb2eb01719266e08117002aba6b51c3f3d79
yyans <1955098530@qq.com>
2022-11-04 17:20:05 +0800

    strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,55 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // let mut left = 0;
+    // let mut right = 0;
+    // for i in 0..input.len() {
+    //     let c = input[i..i+1];
+    //     if c == ' ' {
+    //         left = i;
+    //         break;
+    //     }
+    // }
+    // for i in input.len()..1 {
+    //     let c = input[i..i+1];
+    //     if c == ' ' {
+    //         right = i;
+    //         break;
+    //     }
+    // }
+    // input[left..right].to_string()
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // let s = "cars";
+    // // TODO: Replace "cars" in the string with "balloons"!
+    // let mut left = 0;
+    // let mut right = 0;
+    // for i in 0..input.len() {
+    //     let flag = true;
+    //     for j in i..i+s.len() {
+    //         if input[j] != s[j] {
+    //             flag = false;
+    //             break;
+    //         }
+    //     }
+    //     if flag {
+    //         left = i;
+    //         right = left + s.len();
+    //         break;
+    //     }
+    // }
+    // format!("{}{}{}", input[..left].to_string, "balloons".to_string, input[right+1..])
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
