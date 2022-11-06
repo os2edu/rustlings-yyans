18b6fe37177097a5a045c3b672eb3497ef50d42e
yyans <1955098530@qq.com>
2022-11-06 16:09:19 +0800

    options

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,8 +1,6 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,9 +11,12 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
+        // word = optional_target {
+        //     assert_eq!(word, target);
+        // }
     }
 
     #[test]
@@ -27,10 +28,15 @@ mod tests {
         }
 
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
-        // You can stack `Option<T>`'s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, range);
+        // You can stack `Option<T>`'s into while let and if 
+        while let Some(temp) = optional_integers.pop() {
+
+            if let Some(interger) = temp {
+                assert_eq!(interger, range);
+            }
+
             range -= 1;
         }
+
     }
 }
